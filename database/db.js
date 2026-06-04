5d647f8dacf7c375d9d82c36da413127e7db4538
RicardoUg26 <ugartericardo71@gmail.com>
2026-06-03 23:35:19 -0500

    llamado a la db.js

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const conexion = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
 });
 
 conexion.connect(function(error){
